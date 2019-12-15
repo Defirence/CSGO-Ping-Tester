36db5bd521bf2b43584124dcaf8aae593f81c069
Defirence <defirence680@gmail.com>
2019-12-15 18:25:14 +0200

    Removed non-working ping functions and cleaned up buttons, and ping descriptions on functions.
    Signed-off-by: Defirence <defirence680@gmail.com>

--- build/guimain.py
+++ build/guimain.py
@@ -9,51 +9,51 @@ class Window(Frame):
         self.master = master
         self.init_window()
 
-    #   define function for tkinter_gui_lib window
-
     def init_window(self):
 
-        self.master.title("CSGO Ping Tester v2.0 by Defirence")
+        self.master.title('CSGO Ping Tester v2.1 by Defirence')
 
         self.pack(fill=BOTH, expand=1)
 
-        #   begin adding ESEA Matchmaking (tkinter_gui_lib) buttons
-        #   button label length determines button size
+        #  button label length determines button size
 
         quitButton = Button(self, text="Quit", command=self.client_exit)
         quitButton.place(x=514, y=0)
 
-        eseagermanyoneButton = Button(self, text="ESEA Germany 1", command=self.esea_germany_one)
-        eseagermanyoneButton.place(x=0, y=0)
+        ##############################
+        # BUTTONS FOR ESEA FUNCTIONS #
+        ##############################
 
         eseagermanytwoButton = Button(self, text="ESEA Germany 2", command=self.esea_germany_two)
-        eseagermanytwoButton.place(x=0, y=24)
+        eseagermanytwoButton.place(x=0, y=0)
 
         gboneButton = Button(self, text="ESEA Great Britain 1", command=self.esea_gbone)
-        gboneButton.place(x=0, y=48)
+        gboneButton.place(x=0, y=24)
 
         gbtwoButton = Button(self, text="ESEA Great Britain 2", command=self.esea_gbtwo)
-        gbtwoButton.place(x=0, y=72)
+        gbtwoButton.place(x=0, y=48)
 
         francetwoButton = Button(self, text="ESEA France 2", command=self.esea_francetwo)
-        francetwoButton.place(x=0, y=96)
+        francetwoButton.place(x=0, y=72)
 
         francethreeButton = Button(self, text="ESEA France 3", command=self.esea_francethree)
-        francethreeButton.place(x=0, y=120)
+        francethreeButton.place(x=0, y=96)
 
         francefourButton = Button(self, text="ESEA France 4", command=self.esea_francefour)
-        francefourButton.place(x=0, y=144)
+        francefourButton.place(x=0, y=120)
 
         netherlandsoneButton = Button(self, text="ESEA Netherlands 1", command=self.esea_netherlandsone)
-        netherlandsoneButton.place(x=0, y=168)
+        netherlandsoneButton.place(x=0, y=144)
 
         netherlandstwoButton = Button(self, text="ESEA Netherlands 2", command=self.esea_netherlandstwo)
-        netherlandstwoButton.place(x=0, y=192)
+        netherlandstwoButton.place(x=0, y=168)
 
         swedenButton = Button(self, text="ESEA Sweden", command=self.esea_sweden)
-        swedenButton.place(x=0, y=216)
+        swedenButton.place(x=0, y=192)
 
-        #   begin adding FACEIT Matchmaking (tkinter_gui_lib) buttons below.
+        ##############################
+        # FACEIT Matchmaking Buttons #
+        ##############################
 
         faceitgermanyoneButton = Button(self, text="FACEIT Germany 1", command=self.faceitgermanyone)
         faceitgermanyoneButton.place(x=140, y=0)
@@ -67,101 +67,64 @@ class Window(Frame):
         faceitgbtwoButton = Button(self, text="FACEIT Great Britain 2", command=self.faceitgbtwo)
         faceitgbtwoButton.place(x=140, y=72)
 
-        faceitfranceoneButton = Button(self, text="FACEIT France 1", command=self.faceitfranceone)
-        faceitfranceoneButton.place(x=140, y=96)
-
         faceitfrancetwoButton = Button(self, text="FACEIT France 2", command=self.faceitfrancetwo)
-        faceitfrancetwoButton.place(x=140, y=120)
+        faceitfrancetwoButton.place(x=140, y=96)
 
         faceitfrancethreeButton = Button(self, text="FACEIT France 3", command=self.faceitfrancethree)
-        faceitfrancethreeButton.place(x=140, y=144)
+        faceitfrancethreeButton.place(x=140, y=120)
 
         faceitnetherlandsoneButton = Button(self, text="FACEIT Netherlands 1", command=self.faceitnetherlandsone)
-        faceitnetherlandsoneButton.place(x=140, y=168)
-
-        faceitnetherlandstwoButton = Button(self, text="FACEIT Netherlands 2", command=self.faceitnetherlandstwo)
-        faceitnetherlandstwoButton.place(x=140, y=192)
+        faceitnetherlandsoneButton.place(x=140, y=144)
 
         faceitswedenButton = Button(self, text="FACEIT Sweden", command=self.faceitsweden)
-        faceitswedenButton.place(x=140, y=216)
-
-        faceitsouthafricaoneButton = Button(self, text="FACEIT South Africa One", command=self.faceitsouthafricaone)
-        faceitsouthafricaoneButton.place(x=140, y=240)
-
-        faceitsouthafricatwoButton = Button(self, text="FACEIT South Africa Two", command=self.faceitsouthafricatwo)
-        faceitsouthafricatwoButton.place(x=140, y=264)
-
-        faceitsouthafricathreeButton = Button(self, text="FACEIT South Africa Three",
-                                              command=self.faceitsouthafricathree)
-        faceitsouthafricathreeButton.place(x=140, y=288)
-
-        faceitsouthafricafourButton = Button(self, text="FACEIT South Africa Four", command=self.faceitsouthafricafour)
-        faceitsouthafricafourButton.place(x=140, y=312)
+        faceitswedenButton.place(x=140, y=168)
 
         faceitsouthafricafiveButton = Button(self, text="FACEIT South Africa Five", command=self.faceitsouthafricafive)
-        faceitsouthafricafiveButton.place(x=140, y=336)
-
-        faceitsouthafricasixButton = Button(self, text="FACEIT South Africa Six", command=self.faceitsouthafricasix)
-        faceitsouthafricasixButton.place(x=140, y=360)
+        faceitsouthafricafiveButton.place(x=140, y=192)
 
         faceitsouthafricasevenButton = Button(self, text="FACEIT South Africa Seven",
                                               command=self.faceitsouthafricaseven)
-        faceitsouthafricasevenButton.place(x=140, y=384)
+        faceitsouthafricasevenButton.place(x=140, y=216)
 
-        #   begin adding valve matchmaking (tkinter_gui_lib) buttons below.
+        ###################################
+        # Valve Matchmaking Relay Buttons #
+        ###################################
 
-        valvemm_euw_oneButton = Button(self, text="Valve EU West 1", command=self.valve_euw_one)
+        valvemm_euw_oneButton = Button(self, text="Valve EU West 1 Relay", command=self.valve_euw_one)
         valvemm_euw_oneButton.place(x=308, y=0)
 
-        valvemm_euw_twoButton = Button(self, text="Valve EU West 2", command=self.valve_euw_two)
+        valvemm_euw_twoButton = Button(self, text="Valve EU West 2 Relay", command=self.valve_euw_two)
         valvemm_euw_twoButton.place(x=308, y=24)
 
-        valvemm_eue_oneButton = Button(self, text="Valve EU East", command=self.valve_eue_one)
+        valvemm_eue_oneButton = Button(self, text="Valve EU East Relay 1", command=self.valve_eue_one)
         valvemm_eue_oneButton.place(x=308, y=48)
 
-        valvemm_sweden_oneButton = Button(self, text="Valve Sweden 1", command=self.valve_sweden_one)
-        valvemm_sweden_oneButton.place(x=308, y=72)
+        valvemm_eue_twoButton = Button(self, text="Valve EU East Relay 2", command=self.valve_eue_two)
+        valvemm_eue_twoButton.place(x=308, y=72)
 
-        valvemm_mideastButton = Button(self, text="Valve Middle East", command=self.valve_middleeast)
-        valvemm_mideastButton.place(x=308, y=96)
+        valvemm_sweden_oneButton = Button(self, text="Valve Sweden 1 Relay", command=self.valve_sweden_one)
+        valvemm_sweden_oneButton.place(x=308, y=96)
 
-        valvemm_poland_oneButton = Button(self, text="Valve Poland 1", command=self.valve_poland_one)
-        valvemm_poland_oneButton.place(x=308, y=120)
+        valvemm_mideastButton = Button(self, text="Valve Middle East Relay", command=self.valve_middleeast)
+        valvemm_mideastButton.place(x=308, y=120)
 
-        valvemm_poland_twoButton = Button(self, text="Valve Poland 2", command=self.valve_poland_two)
-        valvemm_poland_twoButton.place(x=308, y=144)
+        valvemm_poland_oneButton = Button(self, text="Valve Poland 1 Relay", command=self.valve_poland_one)
+        valvemm_poland_oneButton.place(x=308, y=144)
 
-        valvemm_spain_oneButton = Button(self, text="Valve Spain 1", command=self.valve_spain_one)
-        valvemm_spain_oneButton.place(x=308, y=168)
+        valvemm_poland_twoButton = Button(self, text="Valve Poland 2 Relay", command=self.valve_poland_two)
+        valvemm_poland_twoButton.place(x=308, y=168)
 
-        valvemm_spain_twoButton = Button(self, text="Valve Spain 2", command=self.valve_spain_two)
-        valvemm_spain_twoButton.place(x=308, y=192)
+        valvemm_southafrica_oneButton = Button(self, text="Valve South Africa 1 Relay",
+                                               command=self.valve_southafrica_one)
+        valvemm_southafrica_oneButton.place(x=308, y=192)
 
-        valvemm_southafrica_oneButton = Button(self, text="Valve South Africa 1", command=self.valve_southafrica_one)
-        valvemm_southafrica_oneButton.place(x=308, y=216)
+        valvemm_southafrica_twoButton = Button(self, text="Valve South Africa 2 Relay",
+                                               command=self.valve_southafrica_two)
+        valvemm_southafrica_twoButton.place(x=308, y=216)
 
-        valvemm_southafrica_twoButton = Button(self, text="Valve South Africa 2", command=self.valve_southafrica_two)
-        valvemm_southafrica_twoButton.place(x=308, y=240)
-
-    #   commented out ESEA Spain because it no longer works
-    #   and also Valve Sweden 2 because there is no reply on the ping
-    #   so that basically leaves a useless function + button
-
-    #   start defining esea server functions below
-
-    def esea_germany_one(self):
-
-        print("Pinging ESEA Germany 1")
-        hostname = "85.131.152.1 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Error, Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
+        ##################
+        # ESEA Functions #
+        ##################
 
     def esea_germany_two(self):
 
@@ -317,9 +280,13 @@ class Window(Frame):
 
         return ping
 
-    #   finish defining esea server functions.
+    #########################
+    # END OF ESEA FUNCTIONS #
+    #########################
 
-    #   start defining faceit server functions below.
+    ####################
+    # FACEIT FUNCTIONS #
+    ####################
 
     def faceitgermanyone(self):
 
@@ -391,20 +358,6 @@ class Window(Frame):
 
         return ping
 
-    def faceitfranceone(self):
-
-        print("Pinging FACEIT France 1")
-        hostname = "62.210.84.115 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
     def faceitfrancetwo(self):
 
         print("Pinging FACEIT France 2")
@@ -447,80 +400,10 @@ class Window(Frame):
 
         return ping
 
-    def faceitnetherlandstwo(self):
-
-        print("Pinging FACEIT Netherlands 2")
-        hostname = "185.16.84.57 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
     def faceitsweden(self):
 
         print("Pinging FACEIT Sweden 1")
         hostname = "185.62.207.33 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def faceitsouthafricaone(self):
-
-        print("Pinging FACEIT South Africa 1")
-        hostname = "165.73.240.107 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def faceitsouthafricatwo(self):
-
-        print("Pinging FACEIT South Africa 2")
-        hostname = "165.73.240.108 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def faceitsouthafricathree(self):
-
-        print("Pinging FACEIT South Africa 3")
-        hostname = "165.73.240.118 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def faceitsouthafricafour(self):
-
-        print("Pinging FACEIT South Africa 4")
-        hostname = "165.73.240.110 -c 4"
         response = os.system("ping " + hostname)
         if response == 0:
             ping = "Ping Successful"
@@ -545,20 +428,6 @@ class Window(Frame):
 
         return ping
 
-    def faceitsouthafricasix(self):
-
-        print("Pinging FACEIT South Africa 6")
-        hostname = "165.73.240.109 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
     def faceitsouthafricaseven(self):
 
         print("Pinging FACEIT South Africa 7")
@@ -573,9 +442,13 @@ class Window(Frame):
 
         return ping
 
-    #   end defining FACEIT server functions here
+    ###########################
+    # END OF FACEIT FUNCTIONS #
+    ###########################
 
-    #   start defining valve matchmaking server functions below
+    ###############################
+    # VALVE MATCHMAKING FUNCTIONS #
+    ###############################
 
     def valve_euw_one(self):
 
@@ -607,7 +480,7 @@ class Window(Frame):
 
     def valve_eue_one(self):
 
-        print("Pinging Valve EUE 1")
+        print("Pinging Valve EUE 1 Relay")
         hostname = "146.66.155.12 -c 4"
         response = os.system("ping " + hostname)
         if response == 0:
@@ -621,7 +494,7 @@ class Window(Frame):
 
     def valve_eue_two(self):
 
-        print("Pinging Valve EUE 2")
+        print("Pinging Valve EUE 2 Relay")
         hostname = "146.66.155.1 -c 4"
         response = os.system("ping " + hostname)
         if response == 0:
@@ -735,34 +608,6 @@ class Window(Frame):
 
         print("Pinging Valve Poland 2")
         hostname = "155.133.241.24 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def valve_spain_one(self):
-
-        print("Pinging Valve Spain 1")
-        hostname = "155.133.246.13 -c 4"
-        response = os.system("ping " + hostname)
-        if response == 0:
-            ping = "Ping Successful"
-        else:
-            ping = "Ping Unsuccessful"
-
-        print(ping)
-
-        return ping
-
-    def valve_spain_two(self):
-
-        print("Pinging Valve Spain 2")
-        hostname = "155.133.247.16 -c 4"
         response = os.system("ping " + hostname)
         if response == 0:
             ping = "Ping Successful"
